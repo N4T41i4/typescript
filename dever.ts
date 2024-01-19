036f73fe833c520d88f405f55e3db87f58e64ae9
Natalia <natalia@example.com>
2024-01-18 23:35:27 -0300

    dever

---
+++ dever.ts
@@ -0,0 +1,21 @@
+
+function multiplicacao(a: number, b: number):number{
+     return a * b;
+}
+
+console.log(multiplicacao( 6,7 ));
+
+
+ 
+interface Nomes{
+    nome :string;
+}
+let individuo ={ 
+    nome:'Natalia',
+}
+
+function saudacao(individuo:Nomes){
+   return "Olá "+ individuo;
+
+    
+}
